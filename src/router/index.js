8f403ca6df554e367cc32de7aef072de1e66c8c6
Walter Gandarella <waltergandarella@yesmkt.com>
2022-11-09 10:35:46 +0000

    router with hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import FormView from '@/views/FormView'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL), // createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
